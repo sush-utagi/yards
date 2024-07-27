fedb93d20099762ee6ba9bdbd5ab4b178ebd73f6
sush-utagi <127123817+sush-utagi@users.noreply.github.com>
2024-07-27 17:15:12 +0800

    Update AddTwoNums.py

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,7 +1,12 @@
-def addTwoNumbers(l1, l2):
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-    l1_reversed = str(l1.val)
-    l2_reversed = str(l2.val)
+
+def addTwoNumbers(l1, l2):
+    l1_reversed = ""
+    l2_reversed = ""
 
     current_node_l1 = l1
     current_node_l2 = l2
@@ -12,6 +17,28 @@ def addTwoNumbers(l1, l2):
     while current_node_l2.next:
         l2_reversed += str(current_node_l2.val)
         current_node_l2 = current_node_l2.next
+
+    l1_reversed += str(current_node_l1.val)
+    l2_reversed += str(current_node_l2.val)
+
+    l1_int = int(l1_reversed[::-1])
+    l2_int = int(l2_reversed[::-1])
+    result_str = str(l1_int + l2_int)
+
+
+    dummy_head = ListNode(0)
+    current = dummy_head
+    for i in reversed(result_str):
+        current.next = ListNode(int(i))
+        current = current.next
+
+    return dummy_head.next
+
+
+        
+
+
+
 
     
 

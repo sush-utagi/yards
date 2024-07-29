98db9f1cecc6808202e9a94a8380961d4f9bd888
sush-utagi <127123817+sush-utagi@users.noreply.github.com>
2024-07-29 16:45:28 +0800

    Update MaximumSizeSubArray.py

--- MaximumSizeSubArray.py
+++ MaximumSizeSubArray.py
@@ -1,25 +1,82 @@
+#   NAIVE FIRST ATTEMPT
+# def lengthOfLongestSubstring(s: str) -> int:
+
+#     max = 0
+#     encountered_letters = set()
+
+#     for i in range(len(s)):
+
+#         encountered_letters.add(s[i])
+#         substring_candidate = s[i]
+
+#         for j in range(i+1, len(s)):
+
+#             if s[j] not in encountered_letters:
+#                 encountered_letters.add(s[j])
+#                 substring_candidate += s[j]
+#                 print(substring_candidate)
+
+#             else:
+#                 encountered_letters.clear()
+#                 break
+
+#         candidate_size = len(substring_candidate)
+#         if candidate_size > max:
+#             max = candidate_size
+    
+#     return max
+    
+
+# testcase = "arwvivbgvtybtnudd"
+# print(lengthOfLongestSubstring(testcase))
+
+
 def lengthOfLongestSubstring(s: str) -> int:
 
-    max = 0
-    encountered_letters = set()
+    window_size = 1
+    i = 0
+    current_window = ""
+    letters_map = {}
 
-    for i in range(len(s)):
 
-        encountered_letters.add(s[i])
-        substring_candidate = str(i)
+    while i < len(s):
+        
+        if s[i] not in letters_map:
+            letters_map[s[i]] = i
+            current_window += s[i]
+            i += 1
+            window_size += 1
 
-        for j in range(i+1, len(s)):
-
-            if s[j] not in encountered_letters:
-                encountered_letters.add(s[j])
-                substring_candidate += s[j]
-
-            else:
-                break
-
-        candidate_size = len(substring_candidate)
-        if candidate_size > max:
-            max = candidate_size
+        else:
+            
+            # Remove all letters from the front of the current_window until the repeated letter
+            while current_window[0] != s[i]:
+                del letters_map[current_window[0]]
+                current_window = current_window[1:]
+            
+            # Remove the repeated letter from the current_window
+            current_window = current_window[1:]
+            
+            # Update the index of the repeated letter in the letters_map
+            letters_map[s[i]] = i
+            
+            # Update the window size
+            window_size = len(current_window)
+            
+            i += 1
     
-    return max
-    
+    return window_size
+
+
+
+        # letters_map = {}
+        # max_length = 0
+        # start = 0
+
+        # for i in range(len(s)):
+        #     if s[i] in letters_map and letters_map[s[i]] >= start:
+        #         start = letters_map[s[i]] + 1
+        #     letters_map[s[i]] = i
+        #     max_length = max(max_length, i - start + 1)
+
+        # return max_length
